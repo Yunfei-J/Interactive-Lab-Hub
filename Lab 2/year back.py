e6ce5ba7fd0e768532f96785121c591c8a1a1cd2
Yunfei-J <yj497@cornell.edu>
2023-09-11 19:18:05 -0400

    Update year back.py

--- Lab 2/year back.py
+++ Lab 2/year back.py
@@ -75,6 +75,7 @@ def delta_sleep(s):
     Parameters:
         s: seconds since elapsed to sleep until
     """
+    draw.text((x, y),'now we stop', font=font, fill="#FFFFFF")
     if int(time.time()) > initial_time + s:
         # check if the delta time has already passed
         return
@@ -103,7 +104,6 @@ while True:
         disp.image(image, rotation)
         current_year -= 5
     elif current_year==1983:
-        now_we_stop()
         delta_sleep(5)
         current_year -= 5
         disp.image(image, rotation)
