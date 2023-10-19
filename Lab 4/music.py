fee4cbec9d600599f11c1467625a4cde52321edf
Yunfei-J <yj497@cornell.edu>
2023-10-18 23:05:31 -0400

    Create music.py

---
+++ Lab 4/music.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sounddevice as sd
+
+# Create a sine wave
+A = 1
+f = 440
+phi = 0
+sr = 44100
+T = 2
+t = np.linspace(0, T, int(sr * T), endpoint=False)
+y = A * np.sin(2 * np.pi * f * t + phi)
+
+# Play the sine wave
+sd.play(y, sr)
+sd.wait()
