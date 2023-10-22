6dc5ba7342afce2202526a79cf50333fb3c2a751
Yunfei-J <yj497@cornell.edu>
2023-10-22 18:40:20 -0400

    Update run_pose_estimation.py

--- Lab 4/run_pose_estimation.py
+++ Lab 4/run_pose_estimation.py
@@ -37,6 +37,24 @@ GPIO.setup(4, GPIO.OUT)
 #button
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+from __future__ import print_function
+import qwiic_proximity
+import time
+import sys
+import sounddevice as sd
+import numpy as np
+
+
+# Initialize parameters
+A = 1  # Amplitude
+frequency = 440  # Fixed frequency
+phi = 0  # Phase
+sr = 44100  # Sample rate
+
+# Start the sound stream
+sd_stream = sd.OutputStream(callback=None, channels=1, samplerate=sr, dtype='float32')
+sd_stream.start()
+
 
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
 # Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
@@ -208,8 +226,23 @@ def get_offsets(output_details, coords, num_key_points=17):
 
 def draw_lines(keypoints, image, bad_pts):
     """connect important body part keypoints with lines"""
-    print(abs(keypoints[0][1]-keypoints[9][1]))
+    distance_value = abs(keypoints[0][1]-keypoints[9][1])
+    print(distance_value)
     # math.dist(keypoints[0],keypoints[9])
+
+    # play sound
+    while True:
+        # proxValue = oProx.get_proximity()
+        # print("Proximity Value: %d" % proxValue)
+        time.sleep(.1)
+        frequency = distance_value*3
+        try:
+            t = np.arange(sr) / sr  # Generate a time vector for one second
+            y = A * np.sin(2 * np.pi * frequency * t + phi).astype('float32')
+            sd_stream.write(y)
+        except KeyboardInterrupt:
+            break
+
     #color = (255, 0, 0)
     color = (0, 255, 0)
     thickness = 2
