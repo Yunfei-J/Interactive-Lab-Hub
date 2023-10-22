ab926494ad28075ae108e983d701765630305297
Yunfei-J <yj497@cornell.edu>
2023-10-21 20:17:11 -0400

    Update get_py_requirements.sh

--- Lab 4/get_py_requirements.sh
+++ Lab 4/get_py_requirements.sh
@@ -3,7 +3,7 @@
 #Code from EdjeElectronics: https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/Raspberry_Pi_Guide.md
 
 #Get GPIO package
-pip3 install RPi.GPIO
+# pip3 install RPi.GPIO
 
 # Get packages required for OpenCV
 
