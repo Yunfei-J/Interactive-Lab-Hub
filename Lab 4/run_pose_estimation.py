55d78315e8c174d531557ef0987379866fe90506
Yunfei-J <yj497@cornell.edu>
2023-10-22 20:20:13 -0400

    Update run_pose_estimation.py

--- Lab 4/run_pose_estimation.py
+++ Lab 4/run_pose_estimation.py
@@ -45,6 +45,72 @@ import sounddevice as sd
 import numpy as np
 
 
+## pi display
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+
+
+
 # Initialize parameters
 A = 1  # Amplitude
 frequency = 440  # Fixed frequency
@@ -239,6 +305,20 @@ def draw_lines(keypoints, image, bad_pts):
     t = np.arange(sr) / sr  # Generate a time vector for one second
     y = A * np.sin(2 * np.pi * frequency * t + phi).astype('float32')
     sd_stream.write(y)
+
+    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+    y = top
+    # display_time = strftime("%m/%d/%Y %H:%M:%S")
+    text_content = "You pulled"
+    text_length = str(distance_value)
+
+    draw.text((x, y), text_content + text_length, font=font, fill="#FFFFFF")
+
+    # Display image.
+    disp.image(image, rotation)
+    time.sleep(1)
+
 
     # while True:
     #     # proxValue = oProx.get_proximity()
