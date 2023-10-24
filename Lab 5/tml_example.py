35f57b544669d9f9fadfbf02916aaaf497dbf7ca
Yunfei-J <yj497@cornell.edu>
2023-10-23 20:28:13 -0400

    Update tml_example.py

--- Lab 5/tml_example.py
+++ Lab 5/tml_example.py
@@ -3,21 +3,21 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-import os
+# import os
 
-# Assuming your TFLite model file is in the same directory as your Python script
-model_filename = os.path.dirname('model.tflite')
+# # Assuming your TFLite model file is in the same directory as your Python script
+# model_filename = 'model.tflite'
 
-# Get the absolute path to the current directory
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# # Get the absolute path to the current directory
+# current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Combine the current directory with the model filename to get the absolute path
-model_path = os.path.join(current_directory, model_filename)
+# # Combine the current directory with the model filename to get the absolute path
+# model_path = os.path.join(current_directory, model_filename)
 
-print(f"Absolute path to TFLite model: {model_path}")
+# print(f"Absolute path to TFLite model: {model_path}")
 
 
-# model_path = 'model.tflite'
+model_path = 'model.tflite'
 image_file_name = "frame.jpg"
 labels_path = "labels.txt"
 
