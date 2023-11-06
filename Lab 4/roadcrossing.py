d85c88a8535d2b7a9c4fd5b55c9d3ed4e9b909d2
Yunfei-J <yj497@cornell.edu>
2023-11-06 00:56:26 -0500

    Update roadcrossing.py

--- Lab 4/roadcrossing.py
+++ Lab 4/roadcrossing.py
@@ -326,7 +326,7 @@ def draw_lines(keypoints, imageCV, bad_pts):
         game_over_sound = pygame.mixer.Sound('myFile.wav')
         pygame.mixer.Sound.set_volume(game_over_sound, 5)
         game_over_sound.play()
-        while (pygame.mixer.music.get_busy()):
+        while (game_over_sound.get_busy()):
             time.sleep(10)
         # if pygame.mixer.music.get_busy() == True:
         #     # print (pygame.mixer.music.get_busy())
