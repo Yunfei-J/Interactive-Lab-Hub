5ac9ec5246d8f7bee8a4b9b27e342a44f4b7efb4
Yunfei-J <yj497@cornell.edu>
2023-11-13 23:06:09 -0500

    Update color.py

--- Lab 6/color.py
+++ Lab 6/color.py
@@ -64,7 +64,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(topic)
 
 
-def on_message(cleint, userdata, msg):
+def on_message(client, userdata, msg):
     # if a message is recieved on the colors topic, parse it and set the color
     if msg.topic == topic:
         colors = list(map(int, msg.payload.decode("UTF-8").split(",")))
