3b4d9c3f6b06e3c3308415da5ee2ae4b6058db05
Yunfei-J <yj497@cornell.edu>
2023-11-13 23:05:15 -0500

    Update color.py

--- Lab 6/color.py
+++ Lab 6/color.py
@@ -68,7 +68,7 @@ def on_message(cleint, userdata, msg):
     # if a message is recieved on the colors topic, parse it and set the color
     if msg.topic == topic:
         colors = list(map(int, msg.payload.decode("UTF-8").split(",")))
-        draw.rectangle((0, 0, width, height * 0.5), fill=(0, 0, 0))
+        draw.rectangle((0, 0, width, height * 0.5), fill=colors)
         disp.image(image)
 
 
