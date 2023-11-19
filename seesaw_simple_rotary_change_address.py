b3ce718ca6c5902f36f81ce211070366f57826d1
Yunfei-J <yj497@cornell.edu>
2023-11-18 23:03:39 -0500

    Update seesaw_simple_rotary_change_address.py

--- seesaw_simple_rotary_change_address.py
+++ seesaw_simple_rotary_change_address.py
@@ -4,21 +4,22 @@ from adafruit_seesaw import seesaw, rotaryio, digitalio
 import time
 import sys
 
-old_address = 0x36 # default
-new_address = 0x37 # must be within 0x07 to 0x78, DEFAULT: 0x72
+old_address = 0x36  # default
+new_address = 0x37  # must be within 0x07 to 0x78, DEFAULT: 0x72
+
 
 def runExample():
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    print("Connected!")
+    qt_enc1 = seesaw.Seesaw(i2c, addr=0x36)
+    qt_enc1.setI2CAddr(0x37)
+    while True:
+        time.sleep(1)  # do nothing
 
-	i2c = board.I2C()  # uses board.SCL and board.SDA
-	print("Connected!")
-	seesaw.setI2CAddr(new_address)
-	# seesaw.setAddress(new_address) # note this updates class member myLCD.address to the new_address
-	while True:
-		time.sleep(1) # do nothing
 
-if __name__ == '__main__':
-	try:
-		runExample()
-	except (KeyboardInterrupt, SystemExit) as exErr:
-		print("\nEnding Example 17")
-		sys.exit(0)
+if __name__ == "__main__":
+    try:
+        runExample()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("\nEnding Example 17")
+        sys.exit(0)
