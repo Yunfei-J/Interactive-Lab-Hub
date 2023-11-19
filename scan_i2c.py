a9dd2dfa4f8e7d591de6a1728854d8cb2340f58b
Yunfei-J <yj497@cornell.edu>
2023-11-18 21:28:13 -0500

    Update scan_i2c.py

--- scan_i2c.py
+++ scan_i2c.py
@@ -3,6 +3,7 @@ import smbus
 # Create an smbus object for the I2C bus (use 1 for Raspberry Pi 3, 4; use 0 for Raspberry Pi 1, 2)
 bus = smbus.SMBus(1)
 
+
 def scan_i2c():
     devices = []
     for address in range(0x03, 0x78):
@@ -12,6 +13,7 @@ def scan_i2c():
         except:
             pass
     return devices
+
 
 # Scan for I2C devices
 found_devices = scan_i2c()
